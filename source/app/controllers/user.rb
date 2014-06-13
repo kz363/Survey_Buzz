2f3e2d9dc707803401be0c9d79701ceaf042838e
Apprentice <kz363@nyu.edu>
2014-06-13 14:18:58 -0400

    Added user controllers and home page view

--- source/app/controllers/user.rb
+++ source/app/controllers/user.rb
@@ -1,3 +1,36 @@
 get '/' do
+  @surveys = Survey.all
+  @user_surveys = Survey.where(id:session[:user_id])
   erb :home
 end
+
+post '/signin' do
+  @user = User.where(email:params[:email])
+  @user.authenticate(params[:password])
+  session[:user_id] = @user.id
+  @name = @user.name
+  content_type :JSON
+  (@name).to_json
+end
+
+post '/signup' do
+  @user = User.create(name:params[:name], email: params[:email], password_hash:params[:password])
+  session[:user_id] = @user.id
+  @name = @user.name
+  content_type :JSON
+  (@name).to_json
+end
+
+# get '/user/surveys' do
+#   @user_surveys = Survey.find_by_id(session[:user_id])
+# end
+
+# get '/surveys' do
+#   @surveys = Survey.all
+# end
+
+get '/signout' do
+  session.clear
+end
+
+
